bbbe4a88cf5bcd10f002934348fe136810fe2355
Farhan21 <farhanabsar21@gmail.com>
2022-02-01 00:21:54 +0600

    use parameter setup

--- src/Components/RecipeList/RecipeList.js
+++ src/Components/RecipeList/RecipeList.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./RecipeList.scss";
 
 const RecipeList = ({recipes}) => {
-    
+    console.log(recipes)
     return (
         <div className='recipe-container'>
             {recipes.map(recipe => 
                 <div className='recipe-card' key={recipe.id}>
+                    <img src={!recipe.photo? `/assets/common.jpg`: `/assets/${recipe.photo}.jpg`} alt={recipe.title} />
                     <h3>{recipe.title}</h3>
-                    <h6>{recipe.cookingTime} to make</h6>
+                    <h6>{recipe.cookingTime} <span className='small-title'>to make</span></h6>
                     <p>{recipe.method.substring(0, 100)}..</p>
+                    <Link to={`/recipes/${recipe.id}`}>see details</Link>
                 </div>
             )}
         </div>
--- src/Pages/Recipe/Recipe.js
+++ src/Pages/Recipe/Recipe.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../Hooks/useFetch';
+import './Recipe.scss';
 
 const Recipe = () => {
+    const { id } = useParams()
+    const url = `http://localhost:3000/recipes/${id}`
+    const { data: recipe, isPending, error } = useFetch(url)
+
     return (
-        <div>
-            
+        <div className='recipe-details-container'>
+           {error && <p className='error'>{error}</p>}
+           {isPending && <p className='loading'>Loading..</p>}
+           {recipe && 
+                <div className='recipe-details'>
+                    <h1>{recipe.title}</h1> 
+                    <h6>{recipe.cookingTime} <span className='small-title'>to make</span></h6>   
+                    <p>{recipe.method}</p>     
+                    <img src={!recipe.photo? `/assets/common.jpg`: `/assets/${recipe.photo}.jpg`} alt={recipe.title}/>
+                </div>
+           }
         </div>
     );
 };
